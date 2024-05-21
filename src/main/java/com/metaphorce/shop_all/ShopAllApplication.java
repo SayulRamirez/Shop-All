729f8e175b21b44c0af8ce20ec351b1072df8e37
SayulRamirez <sayulramirez@gmail.com>
2024-05-20 23:25:47 -0600

    Creando el challenged de Spring boot por Metaphorce

---
+++ src/main/java/com/metaphorce/shop_all/ShopAllApplication.java
@@ -0,0 +1,13 @@
+package com.metaphorce.shop_all;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ShopAllApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(ShopAllApplication.class, args);
+    }
+
+}
