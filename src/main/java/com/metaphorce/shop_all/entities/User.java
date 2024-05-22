fb21b95abf1cea395b26bd57f1036627a228c187
SayulRamirez <sayulramirez@gmail.com>
2024-05-21 21:43:09 -0600

    Creando la entidad de User

---
+++ src/main/java/com/metaphorce/shop_all/entities/User.java
@@ -0,0 +1,25 @@
+package com.metaphorce.shop_all.entities;
+
+import jakarta.persistence.*;
+import lombok.*;
+
+@Builder
+@Getter
+@Setter
+@NoArgsConstructor
+@AllArgsConstructor
+@Entity
+@Table(name = "users")
+public class User {
+
+    @SequenceGenerator(name = "user_sequence", sequenceName = "user_sequence", allocationSize = 1)
+    @GeneratedValue(strategy = GenerationType.SEQUENCE, generator = "user_sequence")
+    @Id
+    private Long id;
+
+    private String name;
+
+    private String email;
+
+    private boolean active;
+}
