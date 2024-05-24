7b6a899a234364e080d5a96a3e8d878024f29f72
SayulRamirez <sayulramirez@gmail.com>
2024-05-24 07:58:06 -0600

    Modificando el método register para que al momento de crear un usuario tambien cree su carrito

--- src/main/java/com/metaphorce/shop_all/services/UserServiceImpl.java
+++ src/main/java/com/metaphorce/shop_all/services/UserServiceImpl.java
@@ -2,7 +2,9 @@ package com.metaphorce.shop_all.services;
 
 import com.metaphorce.shop_all.domain.UserRequest;
 import com.metaphorce.shop_all.domain.UserResponse;
+import com.metaphorce.shop_all.entities.Cart;
 import com.metaphorce.shop_all.entities.User;
+import com.metaphorce.shop_all.repositories.CartRepository;
 import com.metaphorce.shop_all.repositories.UserRepository;
 import com.metaphorce.shop_all.services.interfaces.UserService;
 import jakarta.persistence.EntityExistsException;
@@ -16,9 +18,11 @@ import java.util.List;
 public class UserServiceImpl implements UserService {
 
     private final UserRepository userRepository;
+    private final CartRepository cartRepository;
 
-    public UserServiceImpl(UserRepository repository) {
+    public UserServiceImpl(UserRepository repository, CartRepository cartRepository) {
         this.userRepository = repository;
+        this.cartRepository = cartRepository;
     }
 
     @Override
@@ -44,6 +48,10 @@ public class UserServiceImpl implements UserService {
                 .email(request.email()).build();
 
         userRepository.save(user);
+
+        Cart cart = Cart.builder().user(user).build();
+
+        cartRepository.save(cart);
 
         return new UserResponse(user.getId(), user.getName(), user.getEmail(), user.isActive());
     }
