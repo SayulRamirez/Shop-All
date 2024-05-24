0003f13af7b7d6218ab5250ce5abdf956ed3b844
SayulRamirez <sayulramirez@gmail.com>
2024-05-23 23:56:47 -0600

    Creando el repositorio del User y agregando un método para saber si se encuentra un usuario registrado por medio de su email

---
+++ src/main/java/com/metaphorce/shop_all/repositories/UserRepository.java
@@ -0,0 +1,12 @@
+package com.metaphorce.shop_all.repositories;
+
+import com.metaphorce.shop_all.entities.User;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+@Repository
+public interface UserRepository extends JpaRepository<User, Long> {
+
+
+    boolean existsByEmail(String email);
+}
