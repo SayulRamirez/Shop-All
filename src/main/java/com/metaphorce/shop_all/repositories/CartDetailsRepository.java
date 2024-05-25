44ec79cbeff7ec2968c0e723b201a9ac85ce822f
SayulRamirez <sayulramirez@gmail.com>
2024-05-25 00:28:11 -0600

    Agregado el metodo findCartDetailsByCartId para obtener todos los detalles del carrito

--- src/main/java/com/metaphorce/shop_all/repositories/CartDetailsRepository.java
+++ src/main/java/com/metaphorce/shop_all/repositories/CartDetailsRepository.java
@@ -5,6 +5,7 @@ import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.jpa.repository.Query;
 import org.springframework.stereotype.Repository;
 
+import java.util.List;
 import java.util.Optional;
 
 @Repository
@@ -13,6 +14,9 @@ public interface CartDetailsRepository extends JpaRepository<CartDetails, Long> 
     @Query("select sum(c.numberPieces) from CartDetails c where c.cart.id =:cart")
     Optional<Integer> sumNumberProducts(Long cart);
 
-    @Query("select  sum(c.amount) from CartDetails c where c.cart.id =:cart")
+    @Query("select sum(c.amount) from CartDetails c where c.cart.id =:cart")
     Optional<Double> sumAmount(Long cart);
+
+    @Query("select c from CartDetails c where c.cart.id =:cart")
+    List<CartDetails> findCartDetailsByCartId(Long cart);
 }
