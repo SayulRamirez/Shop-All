1fe83d5f1df256e178699476270a4c68461a2e81
SayulRamirez <sayulramirez@gmail.com>
2024-05-24 23:19:47 -0600

    Cambiado los parametros del metodo addProduct

--- src/main/java/com/metaphorce/shop_all/services/interfaces/CartService.java
+++ src/main/java/com/metaphorce/shop_all/services/interfaces/CartService.java
@@ -1,14 +1,12 @@
 package com.metaphorce.shop_all.services.interfaces;
 
-import com.metaphorce.shop_all.domain.CartResponse;
-import com.metaphorce.shop_all.domain.CartDetailsResponse;
-import com.metaphorce.shop_all.domain.SaleRequest;
+import com.metaphorce.shop_all.domain.*;
 
 import java.util.List;
 
 public interface CartService {
 
-    void addProduct(Long productId, Long userId);
+    void addProduct(AddCartRequest request);
 
     CartResponse getCart(Long userId);
 
@@ -16,5 +14,5 @@ public interface CartService {
 
     void deleteProduct(Long productId, Long userId);
 
-    void shopProducts(SaleRequest saleRequest);
+    SaleResponse shopProducts(SaleRequest saleRequest);
 }
