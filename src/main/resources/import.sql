908e78e4e50738edb6ace2f31f7d4d83264d8396
SayulRamirez <sayulramirez@gmail.com>
2024-05-24 23:12:37 -0600

    Agregado el archivo import.sql para cargar los productos de prueba en la base de datos

---
+++ src/main/resources/import.sql
@@ -0,0 +1,11 @@
+insert into products(category, price, stock, id, code, description) VALUE('CERVEZA', 24.40, 80, 1, '1234', 'Heineken 355 ml lata');
+insert into products(category, price, stock, id, code, description) VALUE('CERVEZA', 22.80, 60, 2, '2345', 'Modelo especial 355 ml lata');
+insert into products(category, price, stock, id, code, description) VALUE('CERVEZA', 23.24, 60, 3, '3456', 'Indio 355 ml ampolleta');
+insert into products(category, price, stock, id, code, description) VALUE('TEQUILA', 240.34, 70, 4, '4567', 'Azul 750 ml botella');
+insert into products(category, price, stock, id, code, description) VALUE('TEQUILA', 310.74, 20, 5, '5678', 'Centenario reposado 750 ml botella');
+insert into products(category, price, stock, id, code, description) VALUE('TEQUILA', 280.64, 40, 6, '6789', 'Azul reposado 750 ml botella');
+insert into products(category, price, stock, id, code, description) VALUE('RON', 320.10, 60, 7, '7890', 'Bacardi 750 ml botella vidrio');
+insert into products(category, price, stock, id, code, description) VALUE('VODKA', 110.60, 30, 8, '8901', 'Kosako 2 litros botella pet');
+insert into products(category, price, stock, id, code, description) VALUE('VODKA', 302.70, 60, 9, '9012', 'Absolut 750 ml botella vidrio');
+insert into products(category, price, stock, id, code, description) VALUE('VINO', 138.40, 20, 10, '0123', 'Lambrusco 750 ml');
+insert into products(category, price, stock, id, code, description) VALUE('MEZCAL', 298.70, 50, 11, '1345', 'Montelobos 750 ml');
