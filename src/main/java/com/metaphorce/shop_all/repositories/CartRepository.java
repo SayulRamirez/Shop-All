54ddf981392fe83d2f6696b54061cb0030fd94ff
SayulRamirez <sayulramirez@gmail.com>
2024-05-24 23:22:18 -0600

    Agregado el metodo findCartByUser para buscar un Cart por el id del usuario

--- src/main/java/com/metaphorce/shop_all/repositories/CartRepository.java
+++ src/main/java/com/metaphorce/shop_all/repositories/CartRepository.java
@@ -2,8 +2,15 @@ package com.metaphorce.shop_all.repositories;
 
 import com.metaphorce.shop_all.entities.Cart;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
 import org.springframework.stereotype.Repository;
+
+import java.util.Optional;
 
 @Repository
 public interface CartRepository extends JpaRepository<Cart, Long> {
+
+    @Query("SELECT c FROM Cart c WHERE c.user.id =:user")
+    Optional<Cart> findCartByUser(Long user);
+
 }
