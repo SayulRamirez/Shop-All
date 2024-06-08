c2cd00a4697c594e6052595582aec4bb06f4695a
SayulRamirez <sayulramirez@gmail.com>
2024-06-08 15:31:21 -0600

    Documentando el endpoint deleteUser

--- src/main/java/com/metaphorce/shop_all/controllers/UserController.java
+++ src/main/java/com/metaphorce/shop_all/controllers/UserController.java
@@ -5,6 +5,7 @@ import com.metaphorce.shop_all.domain.UserResponse;
 import com.metaphorce.shop_all.services.UserServiceImpl;
 import com.metaphorce.shop_all.services.interfaces.UserService;
 import io.swagger.v3.oas.annotations.Operation;
+import io.swagger.v3.oas.annotations.Parameter;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 
@@ -20,17 +21,19 @@ public class UserController {
         this.userService = userService;
     }
 
-    @Operation(summary = "Devuelve todos los usuarios, esten o no esten activos y si no hay, devolverá una lista vacía")
+    @Operation(summary = "Lista todos los usuarios activos e inactivos", description = "Devuelve todos los usuarios, esten o no esten activos y si no hay, devolverá una lista vacía")
     @GetMapping("/getAll")
     public ResponseEntity<List<UserResponse>> getAll() {
         return ResponseEntity.ok(userService.getAll());
     }
 
+    @Operation(summary = "Registra un nuevo usuario", description = "Registra un nuevo usuario y devolvera el usuario registrado, si el usuario ya tiene registrado el email se lanzara un error")
     @PostMapping("/register")
     public ResponseEntity<UserResponse> registerUser(@RequestBody UserRequest request) {
         return ResponseEntity.ok(userService.register(request));
     }
 
+    @Operation(summary = "Inactiva un usuario", description = "Elimina o mejor dicho pone en inactivo al usuario mediante su id, si no lo encuetra lanzara un error.")
     @DeleteMapping(path = "/{id}")
     public ResponseEntity<Void> deleteUser(@PathVariable Long id) {
         userService.deleteUser(id);
--- src/main/java/com/metaphorce/shop_all/domain/UserRequest.java
+++ src/main/java/com/metaphorce/shop_all/domain/UserRequest.java
@@ -1,7 +1,12 @@
 package com.metaphorce.shop_all.domain;
 
+import io.swagger.v3.oas.annotations.media.Schema;
+
 public record UserRequest (
 
+        @Schema(example = "Juan Martín")
         String name,
+
+        @Schema(example = "juan1234@example.com")
         String email
 ){}
