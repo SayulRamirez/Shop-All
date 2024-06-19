adb9ea25ffa8c7dab75783ab9527e6a8aa01585c
SayulRamirez <sayulramirez@gmail.com>
2024-06-18 23:34:33 -0600

    Agregado el mapedo para la excepcion MethodArgumentNotValidException y devolver una respuesta más apropiada

--- src/main/java/com/metaphorce/shop_all/controllers/ExceptionHandlerController.java
+++ src/main/java/com/metaphorce/shop_all/controllers/ExceptionHandlerController.java
@@ -4,14 +4,36 @@ import com.metaphorce.shop_all.domain.ExceptionResponse;
 import jakarta.persistence.EntityExistsException;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
+import org.springframework.validation.FieldError;
+import org.springframework.web.bind.MethodArgumentNotValidException;
 import org.springframework.web.bind.annotation.ControllerAdvice;
 import org.springframework.web.bind.annotation.ExceptionHandler;
 
 import java.time.ZoneId;
 import java.time.ZonedDateTime;
+import java.util.LinkedHashMap;
+import java.util.Map;
 
 @ControllerAdvice
 public class ExceptionHandlerController {
+
+    @ExceptionHandler(MethodArgumentNotValidException.class)
+    protected ResponseEntity<Map<String, Object>> handleMethodArgumentNotValidException(MethodArgumentNotValidException exception) {
+
+        Map<String, Object> response = new LinkedHashMap<>();
+
+        exception.getBindingResult().getAllErrors().forEach((error) -> {
+            String field = ((FieldError) error).getField();
+            String message = error.getDefaultMessage();
+
+            response.put(field, message);
+        });
+
+        response.put("timestamp", ZonedDateTime.now(ZoneId.of("America/Chicago")));
+        response.put("status", HttpStatus.BAD_REQUEST);
+
+        return ResponseEntity.badRequest().body(response);
+    }
 
     @ExceptionHandler(EntityExistsException.class)
     public ResponseEntity<ExceptionResponse> handleEntityExistsException(EntityExistsException exception) {
