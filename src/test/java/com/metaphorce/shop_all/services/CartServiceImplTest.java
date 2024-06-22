f595a8332718bbbf7eee497833be659f4595de8a
SayulRamirez <sayulramirez@gmail.com>
2024-06-21 22:04:24 -0600

    Agregando el caso de prueba cuadno no se cuenta con suficiente existencias a l agregar un producto al carrito

--- src/test/java/com/metaphorce/shop_all/services/CartServiceImplTest.java
+++ src/test/java/com/metaphorce/shop_all/services/CartServiceImplTest.java
@@ -1,6 +1,8 @@
 package com.metaphorce.shop_all.services;
 
 import com.metaphorce.shop_all.domain.AddCartRequest;
+import com.metaphorce.shop_all.entities.Product;
+import com.metaphorce.shop_all.exceptions.NotEnoughStockException;
 import com.metaphorce.shop_all.repositories.CartDetailsRepository;
 import com.metaphorce.shop_all.repositories.CartRepository;
 import com.metaphorce.shop_all.repositories.ProductRepository;
@@ -63,4 +65,26 @@ public class CartServiceImplTest {
         verify(productRepository, times(1)).findById(any(Long.class));
         verify(cartRepository, never()).findCartByUser(any(Long.class));
     }
+
+    @Test
+    void whenThereIsInefficientStock() {
+
+        AddCartRequest request = new AddCartRequest(1L, 1L, 3);
+        Product product = Product.builder()
+                        .id(1L).stock(2).build();
+
+        when(userRepository.existsUserByIdAndActiveIsTrue(any(Long.class))).thenReturn(true);
+
+        when(productRepository.findById(any(Long.class))).thenReturn(Optional.of(product));
+
+        assertThrows(NotEnoughStockException.class, () -> underTest.addProduct(request));
+        verify(userRepository, times(1)).existsUserByIdAndActiveIsTrue(any(Long.class));
+        verify(productRepository, times(1)).findById(any(Long.class));
+        verify(cartRepository, never()).findCartByUser(any(Long.class));
+    }
+
 }
+
+// cuando el producto no esta en el carrito
+
+// cuando el producto ya esta en el carrito
