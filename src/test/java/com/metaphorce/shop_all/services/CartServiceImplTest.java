7df12369591c7fe6af46cfa6bf42f4cfc10cb010
SayulRamirez <sayulramirez@gmail.com>
2024-06-21 23:02:44 -0600

    Agregado el caso de prueba cuando se agrega exitosamente un producto nuevo al carrito

--- src/test/java/com/metaphorce/shop_all/services/CartServiceImplTest.java
+++ src/test/java/com/metaphorce/shop_all/services/CartServiceImplTest.java
@@ -1,7 +1,10 @@
 package com.metaphorce.shop_all.services;
 
 import com.metaphorce.shop_all.domain.AddCartRequest;
+import com.metaphorce.shop_all.entities.Cart;
+import com.metaphorce.shop_all.entities.CartDetails;
 import com.metaphorce.shop_all.entities.Product;
+import com.metaphorce.shop_all.entities.User;
 import com.metaphorce.shop_all.exceptions.NotEnoughStockException;
 import com.metaphorce.shop_all.repositories.CartDetailsRepository;
 import com.metaphorce.shop_all.repositories.CartRepository;
@@ -83,8 +86,29 @@ public class CartServiceImplTest {
         verify(cartRepository, never()).findCartByUser(any(Long.class));
     }
 
+    // cuando el producto no esta en el carrito
+    @Test
+    void whenTheProductIsNotIntTheCart() {
+
+        AddCartRequest request = new AddCartRequest(1L, 1L, 3);
+        Product product = Product.builder()
+                .id(1L).stock(6).price(24.40).build();
+
+        Cart cart = Cart.builder().id(1L).user(User.builder().id(1L).build()).numberProducts(1).amount(24.40).build();
+
+        when(userRepository.existsUserByIdAndActiveIsTrue(any(Long.class))).thenReturn(true);
+
+        when(productRepository.findById(any(Long.class))).thenReturn(Optional.of(product));
+
+        when(cartRepository.findCartByUser(any(Long.class))).thenReturn(Optional.of(cart));
+
+        when(cartDetailsRepository.getId(any(Long.class), any(Long.class))).thenReturn(Optional.of(1L));
+
+        when(cartDetailsRepository.findById(any(Long.class))).thenReturn(Optional.of(CartDetails.builder().build()));
+
+        underTest.addProduct(request);
+
+        verify(cartDetailsRepository, times(1)).save(any(CartDetails.class));
+    }
 }
-
-// cuando el producto no esta en el carrito
-
 // cuando el producto ya esta en el carrito
