734896dc13c5f4b5ce2f3b3620b2dff88332df74
SayulRamirez <sayulramirez@gmail.com>
2024-06-22 20:37:55 -0600

    Agregando los ejemplos que se mostraran en la documentacion de swagger

--- src/main/java/com/metaphorce/shop_all/domain/ProductResponse.java
+++ src/main/java/com/metaphorce/shop_all/domain/ProductResponse.java
@@ -1,15 +1,20 @@
 package com.metaphorce.shop_all.domain;
 
 import com.metaphorce.shop_all.enums.Category;
+import io.swagger.v3.oas.annotations.media.Schema;
 
 public record ProductResponse(
 
+        @Schema(example = "Lambrusco 750 ml")
         String description,
 
+        @Schema(example = "VINO")
         Category category,
 
+        @Schema(example = "20")
         Integer stock,
 
+        @Schema(example = "138.40")
         Double price
 ) {
 }
